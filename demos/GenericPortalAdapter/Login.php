7125ae2060fcf3eda03bd5d6ea016930f783fee7
Vasil Dakov <vasildakov@gmail.com>
2014-04-06 20:31:07 +0200

    updated demos

--- demos/GenericPortalAdapter/Login.php
+++ demos/GenericPortalAdapter/Login.php
@@ -3,14 +3,23 @@ namespace VasilDakov;
 
 include "../../src/GDS/GenericPortalAdapter.php";
 include "../../src/GDS/GenericPortalAdapter/LoginRequest.php";
+include "../../src/GDS/GenericPortalAdapter/LoginResponse.php";
 
 use VasilDakov\GDS\GenericPortalAdapter;
 use	VasilDakov\GDS\GenericPortalAdapter\LoginRequest;
+use	VasilDakov\GDS\GenericPortalAdapter\LoginResponse;
 
 $wsdl = '../../data/GenericPortalAdapter.wsdl';
 $client = new GenericPortalAdapter($wsdl, $options = array());
 
 $request = new LoginRequest;
+$request->systemUID = 112233;
+$request->clientUID = 9988;
+$request->clientIpAddress = '10.1.1.1';
+$request->username = "username";
+$request->password = "xxxxxxxxxxxx"
 
+$response = new LoginResponse;
 $response = $client->Login($request);
-var_dump($response);
+
+var_dump($response->Result);
